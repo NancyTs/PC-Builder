bdd40c14f4e8adb0585e37ee845de687c58a960f
Nancy Ts <nancy.tsr1@gmail.com>
2021-03-27 13:45:13 +0200

    Test API Call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,38 @@ class HomeController < ApplicationController
 
   def about
   end
+
+
+  def search
+    parts = find_part(params[:part])
+
+    unless parts
+      flash[:notice] = 'Part not found'
+      return render action: :index
+    end
+
+    @part = parts['items']
+  end
+
+  private
+
+  def find_part(name)
+    request_api(
+      "https://api.apigenius.io/products/search?keyword=#{URI.encode(name)}"
+    )
+  end
+
+  def request_api(url)
+    response = Excon.get(
+      url,
+      headers: {
+        'ApiGenius_API_Key' => ENV.fetch('ApiGenius_API_Key')
+      }
+    )
+
+    return nil if response.status != 200
+
+    JSON.parse(response.body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :computers
-  #get 'home/index'
+  get 'home/index'
   get 'home/about'
   #root 'home#index'
   root 'computers#index'
+  get '/search' => 'home#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
